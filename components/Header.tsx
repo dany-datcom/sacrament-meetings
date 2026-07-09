372be664596bfaa32bae87d7bc0acf5913ac0bd8
dany-datcom <djimenezgonzalez@byupathway.com>
2026-07-08 21:25:47 -0600

    sacrament meetings

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NavLinks from './NavLinks';
+
+export default function Header() {
+  return (
+    <header className="bg-white shadow">
+      <NavLinks />
+    </header>
+  );
+}
